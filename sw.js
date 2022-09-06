b4d7088a077127b3e2d8917e5e76f2de0092594f
Gabriel Antonio <110503233+Gabriel-Antonio-Souto@users.noreply.github.com>
2022-09-06 17:48:35 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,49 @@
+const cacheName = 'rabies'
+
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache){
+            cache.addAll([
+                './',
+                './index.html',
+                './manifest.webmanifest',
+                './index.js'
+            ])
+        })
+    )
+    return self.skipWaiting()
+})
+
+self.addEventListener('activate', e =>{
+    self.clients.claim()
+})
+
+self.addEventListener('fetch', async e =>{
+    const req = e.request
+    const url = new URL(req.url)
+
+    if(url.origin === location.origin){
+        e.respondWith(cacheFirst(req))
+    } else{
+        e.respondWith(networkAndCache(req))
+    }
+})
+
+async function cacheFirst(req){
+    const cache = await caches.open(cacheName)
+    const cached = await cache.match(req)
+
+    return cached || fetch(req)
+}
+
+async function networkAndCache(req){
+    const cache = await caches.open(cacheName);
+    try{
+        const refresh = await fetch(req)
+        await cache.put(req, fresh.clone())
+        return refresh
+    } catch(e){
+        const cached = await cache.match(req);
+        return cached
+    }
+}
